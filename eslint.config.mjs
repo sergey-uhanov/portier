a4ab7bbbf941b8f4a87ee5ed142346fb34aa9c00
sergey ihanov <uhanov.sergio@gmail.com>
2024-12-20 16:41:00 +0200

    fix code review

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,12 @@
 // @ts-check
 
 import eslint from '@eslint/js';
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
 
 export default [
 	{
 		// Настройки языка и парсера
 		languageOptions: {
-			parser: tsParser,
+
 			ecmaVersion: 'latest', // Используем последний стандарт ECMAScript
 			sourceType: 'module', // Модульный формат
 			globals: {
@@ -20,7 +18,7 @@ export default [
 		},
 		files: ['**/*.ts', '**/*.tsx'], // Добавьте паттерны для TypeScript-файлов
 		plugins: {
-			'@typescript-eslint': tseslint,
+
 		},
 		env: {
 			browser: true, // Для браузерных API
@@ -29,7 +27,6 @@ export default [
 		},
 		rules: {
 			...eslint.configs.recommended.rules, // Базовые правила ESLint
-			...tseslint.configs.recommended.rules, // Базовые правила TypeScript-ESLint
 			// Ваши кастомные правила
 			'no-console': 'warn', // Предупреждать о console.log
 			'@typescript-eslint/no-unused-vars': 'off',
